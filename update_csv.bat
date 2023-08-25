3d9bb6a454debb8a810d06804b01800fdf4301c0
niknafidz <140383663+niknafidz@users.noreply.github.com>
2023-08-25 09:51:04 +0800

    Update update_csv.bat

--- update_csv.bat
+++ update_csv.bat
@@ -1,6 +1,1 @@
-@echo off
-"C:\Program Files (x86)\WinSCP\WinSCP.exe" ^
-/command ^
-"open sftp://username:password@your-server-ip" ^
-"put C:\path\to\local\csv\file.csv /path/to/remote/csv/file.csv" ^
-"exit"
+
